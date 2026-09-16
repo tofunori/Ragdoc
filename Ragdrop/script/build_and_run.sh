a9e3fab9a140ae7b0e6ae01ec0c31097e0aa4bcb
Thierry <tofunori@MacBookPro-de-Thierry.local>
2026-09-16 09:27:39 -0400

    Use selected layered Ragdrop icon

--- Ragdrop/script/build_and_run.sh
+++ Ragdrop/script/build_and_run.sh
@@ -42,7 +42,7 @@ cat >"$INFO_PLIST" <<PLIST
   <key>CFBundleShortVersionString</key>
   <string>0.2</string>
   <key>CFBundleVersion</key>
-  <string>20</string>
+  <string>21</string>
   <key>CFBundleIconFile</key>
   <string>RagdropIcon</string>
   <key>CFBundlePackageType</key>
